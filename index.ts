375faa4e39a9b13c437dcdf0687e6ef774ffafcd
Davide Pedron <davide@happydev.cloud>
2021-06-09 01:29:31 +0200

    Add ESLint, update all dependencies

--- index.ts
+++
@@ -1,7 +0,0 @@
-import Phaser = require("phaser");
-
-const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO
-}
-
-export default new Phaser.Game(config);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Phaser from 'phaser';
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO
+}
+
+document.addEventListener("DOMContentLoaded", () => new Phaser.Game(config));
